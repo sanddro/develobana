9a2bf8c15892fd4b8b75e6bb0fe1e5bcd5c60e49
Vache Katsadze <vkats15@freeuni.edu.ge>
2018-07-06 03:07:16 +0400

    Added router to react. Created register and login components' routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Login from './components/login/Login';
+import Register from './components/login/Register';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div>test</div>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route exact path="/login" component={ Login } />
+          <Route exact path="/register" component={ Register } />
+        </div>
+      </BrowserRouter>
     );
   }
 }
