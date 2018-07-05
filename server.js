528e3267b4a02cb708e947bf56b6e765074c3ea2
Sandro Jikia <jikiasandro@gmail.com>
2018-07-06 02:14:43 +0400

    Created react client. Set up running both server and client with one command: npm run dev

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import keys from './config/keys';
 import user from './routes/user';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
